de0842edec13280fb76461291ddbf638fb1dc869
Uliana Kolodii <ulianakolodii@gmail.com>
2023-05-26 17:17:13 +0300

    feat: refactor shop items array in preparation for new feature
    - Add 6 new items to the mockedShopItems array
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/mockedData.ts
+++ src/app/mockedData.ts
@@ -5,18 +5,13 @@ export type shopItemType = {
 };
 
 const mockedShopItems: Array<shopItemType> = [
-  { img: "/burger.png", title: "Burger", price: "5.00$" },
-  { img: "/fries.png", title: "Fries", price: "2.20$" },
-  { img: "/hotdog.png", title: "Hotdog", price: "3.00$" },
-  { img: "/taco.png", title: "Taco", price: "4.00$" },
-  { img: "/pizza.png", title: "Pizza", price: "8.00$" },
-  { img: "/donut.png", title: "Donut", price: "1.00$" },
-  { img: "/popcorn.png", title: "Popcorn", price: "2.00$" },
-  { img: "/coke.png", title: "Coke", price: "1.60$" },
-  { img: "/cake.png", title: "Cake", price: "11.00$" },
-  { img: "/icecream.png", title: "Icecream", price: "6.00$" },
-  { img: "/cookie.png", title: "Cookie", price: "4.50$" },
-  { img: "/flan.png", title: "Flan", price: "8.15$" },
+  { img: "/lava.webp", title: "Lava", price: "240$" },
+  { img: "/defund.webp", title: "DeFund", price: "230$" },
+  { img: "/chainflip.webp", title: "Chainflip", price: "240$" },
+  { img: "/shardeum.webp", title: "Shardeum", price: "240$" },
+  { img: "/muon-network.webp", title: "Muon Network", price: "230$" },
+  { img: "/massa.webp", title: "Massa", price: "240$" },
+  { img: "/elixir-finance.webp", title: "Elixir.finance", price: "240$" },
 ];
 
 export const shopItems = () => {
