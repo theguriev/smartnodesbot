2823acd391024574524a3a591ca5479e480bf2e9
Uliana Kolodii <ulianakolodii@gmail.com>
2023-05-26 17:20:02 +0300

    feat: use uuidv4 to generate unique keys in List.tsx
    - Add the `uuidv4` package to `package.json`
    - Use `uuidv4` to generate unique keys for `ListItem` components in `List.tsx`
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/List.tsx
+++ src/app/List.tsx
@@ -1,6 +1,7 @@
 import ListItem from "./ListItem";
 import { shopItems } from "./mockedData";
 import { shopItemType } from "./mockedData";
+import { v4 as uuidv4 } from "uuid";
 
 export default function List() {
   const shopItemsList = shopItems();
@@ -8,7 +9,7 @@ export default function List() {
   return (
     <div className="max-w-[480px] flex flex-wrap justify-around content-end">
       {shopItemsList.map((shopItem: shopItemType) => (
-        <ListItem key={shopItem.price} {...shopItem} />
+        <ListItem key={uuidv4()} {...shopItem} />
       ))}
     </div>
   );
