172528c9e4d3f6795f647f0de5898adad77d61a5
Uliana Kolodii <ulianakolodii@gmail.com>
2023-05-30 11:06:42 +0300

    refactor: refactor Title component to use themeParams and improve code readability
    - Refactor Title component to use themeParams from the window object
    - Change span style to use optional chaining
    - Remove unused code block in Title component
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/Title.tsx
+++ src/app/Title.tsx
@@ -1,19 +1,17 @@
 import { FC } from "react";
 
-const themeParams = typeof window !== "undefined" ? window?.Telegram?.WebApp?.themeParams : {};
+const Title: FC<{ title: string; price: string }> = ({ title, price }) => {
+  const themeParams = typeof window !== "undefined" ? window?.Telegram?.WebApp?.themeParams : {};
 
-if (window === undefined) {
-
-}
-
-const Title: FC<{ title: string; price: string }> = ({ title, price }) => (
-  <div className="text-[13px] flex items-center gap-1">
-    <span style={{ color: themeParams.text_color }}>{title}</span>
-    <span style={{ color: themeParams.text_color }}>·</span>
-    <span className="font-bold" style={{ color: themeParams.text_color }}>
-      {price}
-    </span>
-  </div>
-);
+  return (
+    <div className="text-[13px] flex items-center gap-1">
+      <span style={{ color: themeParams?.text_color }}>{title}</span>
+      <span style={{ color: themeParams?.text_color }}>·</span>
+      <span className="font-bold" style={{ color: themeParams?.text_color }}>
+        {price}
+      </span>
+    </div>
+  );
+};
 
 export default Title;
