eb6d2ee022e46af8b02cc1c9127477a1de739a1e
Uliana Kolodii <ulianakolodii@gmail.com>
2023-05-30 17:25:16 +0300

    refactor: refactor Title component styling for improved performance
    - Remove textColor state and useEffect from Title component
    - Change div className to style object with color property in Title component
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/Title.tsx
+++ src/app/Title.tsx
@@ -3,20 +3,11 @@
 import { FC, useState, useEffect } from "react";
 
 const Title: FC<{ title: string; price: string }> = ({ title, price }) => {
-  const [textColor, setTextColor] = useState<string>("");
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const color = window?.Telegram?.WebApp?.themeParams?.text_color;
-      if (color && color === "#ffffff") {
-        setTextColor("text-white");
-      }
-      else setTextColor("text-black");
-    }
-  }, []);
-
   return (
-    <div className={`${textColor} text-[13px] flex items-center gap-1`}>
+    <div
+      className={`text-[13px] flex items-center gap-1`}
+      style={{ color: window?.Telegram?.WebApp?.themeParams?.text_color }}
+    >
       <span>{title}</span>
       <span>·</span>
       <span>{price}</span>
