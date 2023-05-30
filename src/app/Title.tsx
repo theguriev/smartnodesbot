772cdaff7597bab62b4d9157ac8bd7be55e2005a
Uliana Kolodii <ulianakolodii@gmail.com>
2023-05-30 14:43:50 +0300

    refactor: refactor text color management using hooks and classes
    - Remove unused import of `use client`
    - Use state and useEffect hooks to set text color dynamically
    - Remove inline style and use class name for text color
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/Title.tsx
+++ src/app/Title.tsx
@@ -1,17 +1,24 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState, useEffect } from "react";
 
 const Title: FC<{ title: string; price: string }> = ({ title, price }) => {
-  const themeParams =
-    typeof window !== "undefined" ? window?.Telegram?.WebApp?.themeParams : {};
-  const textColor = "color-" + themeParams?.text_color;
+  const [textColor, setTextColor] = useState<string>("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const color = window?.Telegram?.WebApp?.themeParams?.text_color;
+      if (color) {
+        setTextColor("color-" + color);
+      }
+    }
+  }, []);
 
   return (
     <div className="text-[13px] flex items-center gap-1">
       <span className={textColor}>{title}</span>
       <span className={textColor}>·</span>
-      <span className="font-bold" style={{ color: themeParams?.text_color }}>
-        {price}
-      </span>
+      <span className="font-bold">{price}</span>
     </div>
   );
 };
