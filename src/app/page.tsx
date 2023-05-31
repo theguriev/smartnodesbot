f6232d7f61b72b3cc100eccdebd7c62a9f9743d0
Uliana Kolodii <ulianakolodii@gmail.com>
2023-05-31 16:20:33 +0300

    refactor: remove unused code and imports
    - Remove an unused import statement
    - Remove a line of unused code for routing
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-"use client"
+"use client";
 import Script from "next/script";
 import { useState } from "react";
 import List from "./List";
-import { useRouter } from "next/navigation";
 
 const Home = () => {
   const [ready, setReady] = useState(false);
   const handleReady = () => {
     setReady(true);
   };
-  const router = useRouter();
-  window?.Telegram?.WebApp?.MainButton.onClick(() => router.push("/cart"));
+
   return (
     <>
       <Script
