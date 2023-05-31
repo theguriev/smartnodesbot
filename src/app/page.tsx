dd90d0b5d3233410235b074c59ea6dd152ef9f41
Uliana Kolodii <ulianakolodii@gmail.com>
2023-05-31 11:20:50 +0300

    feat: refactor shop items display into separate component
    - Create a new file `List.tsx` and add a list of mocked shop items
    - Remove the `mockedShopItems` list from `page.tsx`
    - Replace `ListItem` component with `List` component in `page.tsx`
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,27 @@
 import Script from "next/script";
-import ListItem from "./ListItem";
-import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import List from "./List";
 
-const mockedShopItems = [
-  { img: "/lava.webp", title: "Lava", price: "240$" },
-  { img: "/defund.webp", title: "DeFund", price: "230$" },
-  { img: "/chainflip.webp", title: "Chainflip", price: "240$" },
-  { img: "/shardeum.webp", title: "Shardeum", price: "240$" },
-  { img: "/muon-network.webp", title: "Muon", price: "230$" },
-  { img: "/massa.webp", title: "Massa", price: "240$" },
-  { img: "/elixir-finance.webp", title: "Elixir", price: "240$" },
-];
-
-const Home = () => (
-  <>
-    <Script src="https://telegram.org/js/telegram-web-app.js" />
-    <div className="flex justify-center">
-      <div className="max-w-[360px] flex justify-center">
-        <div className="flex flex-wrap justify-start">
-          {mockedShopItems.map((shopItem) => (
-            <ListItem key={uuidv4()} {...shopItem} />
-          ))}
+const Home = () => {
+  const [ready, setReady] = useState(false);
+  const handleReady = () => {
+    setReady(true);
+  };
+  return (
+    <>
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        onReady={handleReady}
+      />
+      {ready && (
+        <div className="flex justify-center">
+          <div className="max-w-[360px] flex justify-center">
+            <List />
+          </div>
         </div>
-      </div>
-    </div>
-  </>
-);
+      )}
+    </>
+  );
+};
 
 export default Home;
