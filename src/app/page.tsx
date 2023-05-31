9f8fa502081f267dfdab397abaf5e47e760ec849
Uliana Kolodii <ulianakolodii@gmail.com>
2023-05-31 11:20:50 +0300

    feat: refactor shopping page to use reusable List component
    - Add a new file `src/app/List.tsx`
    - Remove `mockedShopItems` from `src/app/page.tsx`
    - Import `List` component in `src/app/page.tsx`
    - Render `List` component instead of `mockedShopItems` in `src/app/page.tsx`
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,7 @@
 import Script from "next/script";
 import { useState } from "react";
 import ListItem from "./ListItem";
-import { v4 as uuidv4 } from "uuid";
-
-const mockedShopItems = [
-  { img: "/lava.webp", title: "Lava", price: "240$" },
-  { img: "/defund.webp", title: "DeFund", price: "230$" },
-  { img: "/chainflip.webp", title: "Chainflip", price: "240$" },
-  { img: "/shardeum.webp", title: "Shardeum", price: "240$" },
-  { img: "/muon-network.webp", title: "Muon", price: "230$" },
-  { img: "/massa.webp", title: "Massa", price: "240$" },
-  { img: "/elixir-finance.webp", title: "Elixir", price: "240$" },
-];
+import List from "./List";
 
 const Home = () => {
   const [ready, setReady] = useState(false);
@@ -28,11 +18,7 @@ const Home = () => {
       {ready && (
         <div className="flex justify-center">
           <div className="max-w-[360px] flex justify-center">
-            <div className="flex flex-wrap justify-start">
-              {mockedShopItems.map((shopItem) => (
-                <ListItem key={uuidv4()} {...shopItem} />
-              ))}
-            </div>
+            <List />
           </div>
         </div>
       )}
