519f7528324fc69393c1d5eb30aaa17673162691
Uliana Kolodii <ulianakolodii@gmail.com>
2023-06-05 12:08:00 +0300

    feat: add shop items promise

---
+++ src/app/api/shopItems.tsx
@@ -0,0 +1,94 @@
+export const fetchShopItems = () => {
+  return new Promise((resolve) => {
+    const shopItems = [
+      {
+        id: 5,
+        name: "Humans",
+        descriptionEn:
+          "HUMANS is envisioned as a platform that can be adapted and tailored to work with any service or product vertical in the market. An all-in-one technology solution for AI-as-a-Service, the HUMANS platform will combine traditional solutions with blockchain-based record-keeping and governance. AI Developers have a new way to deploy and train their algorithms in a ready-to-sell environment where they can share their creations and collaboratively improve their models. AI models may be optimized for specific data inputs. This may happen on a self/unsupervised basis, which requires no labels, or a supervised basis which requires labels. This training can be automated (continuous integration style) or configured manually (data+model) resulting in a trained model taking the form of a binary file. Apps are a combination of AIs that generate valuable outputs on request. Each request made to an App is sent to each of its components, which in turn share any proceeds respectively. Due to technical limitations, there may be a limited number of slots available for high performing Apps on the platform. Apps may be built based on the framework available in Humans Studio: - From scratch, built to serve a specific or unique use case - From a recipe made available in the Humans Studio Users will have the ability to deploy Apps at scale in the HUMANS decentralized ecosystem and to access a host of decentralized services provided by other members of the community. In order to go live with an App and make it available more broadly in the system, users will be required to stake $HEART tokens.",
+        descriptionRu:
+          "HUMANS задуман как платформа, которую можно адаптировать и настроить для работы с любой услугой или продуктом на рынке. Универсальное технологическое решение для AI как услуги, платформа HUMANS будет сочетать традиционные решения с ведением учета и управлением на основе блокчейна. У разработчиков ИИ есть новый способ развертывания и обучения своих алгоритмов в готовой к продаже среде, где они могут делиться своими творениями и совместно улучшать свои модели. Модели ИИ могут быть оптимизированы для конкретных входных данных. Это может происходить на основе самоконтроля/неконтролируемой работы, которая не требует меток, или на контролируемой основе, требующей меток. Это обучение может быть автоматизировано (стиль непрерывной интеграции) или настроено вручную (данные + модель), в результате чего обученная модель принимает форму двоичного файла. Приложения представляют собой комбинацию ИИ, которые генерируют ценные результаты по запросу. Каждый запрос, сделанный в приложении, отправляется каждому из его компонентов, которые, в свою очередь, соответственно распределяют любые доходы. Из-за технических ограничений, на платформе может быть ограниченное количество слотов для высокопроизводительных приложений. Приложения могут быть созданы на основе фреймворка, доступного в Humans Studio: - Создан с нуля для конкретного или уникального варианта использования - Благодаря рецепту, доступному в Humans Studio, пользователи смогут развертывать приложения в масштабе децентрализованной экосистемы HUMANS и получать доступ к множеству децентрализованных услуг, предоставляемых другими членами сообщества. Чтобы запустить приложение и сделать его более доступным в системе, пользователям необходимо будет размещать токены $HEART.",
+        blockQuoteEn:
+          "Humans is a next-generation sovereign emerging platform found in an ecosystem owned by a third party using AI to create large-scale projects.",
+        blockQuoteRu:
+          "Humans - это суверенная платформа блокчейна нового поколения, которая объединяет экосистему заинтересованных сторон вокруг использования искусственного интеллекта для создания масштабных проектов.",
+        url: "https://humans.ai/",
+        chain: "COSMOS",
+        installationComplexity: "FIVE",
+        status: "ACTIVE",
+        rating: "FIVE",
+        fieldOfActivity: "Blockchain",
+        valid: true,
+        monthlyPrice: 340,
+        walletAddress: "",
+        imageUrl: "",
+        logo: "",
+        twitter: "https://twitter.com/humansdotai",
+        telegram: "https://t.me/humansdotai",
+        discord: "",
+        reddit: "",
+        medium: "https://medium.com/humansdotai",
+        github: "",
+      },
+      {
+        id: 1,
+        name: "Onomy2",
+        descriptionEn:
+          "Onomy consists of four pillars designed to be a self-governed monetary stabilization system. Onomy Network (ONET): An application specific Layer 1 blockchain leveraging Tendermint BFT consensus. Powers bi-directional bridges to prominent blockchains in and outside of the Cosmos ecosystem - such as Near, Aurora, Avalanche, Polygon, and more. Onomy Exchange (ONEX): A decentralized exchange that aims to replicate a traditional centralized exchange experience to settle the world's high-volume demands on-chain, decentralized, and non-custodially. It combines AMM Liquidity Pools with an Orderbook UI, enabling Liquidity Providers to earn yield and traders to engage in familiar orderbook trading strategies supporting market, limit, stop, and conditional orders. The ONEX will be both cross-chain and multi-chain. Paired with Onomy Access and Onomy's bridge network, ONEX enables users to seamlessly trade native assets between blockchains - or trade assets native to one specific blockchain. Onomy Access: A non-custodial multi-chain mobile wallet app through which users may manage all assets from integrated blockchains. This includes staking, governance, transferring assets, and even viewing your NFT collections from multiple blockchains - all on one singular wallet app. Access makes cross-chain and multi-chain user experience seamless. Connect to dApps by scanning a QR code - gone are the days of connecting various browser extensions to access Web3.",
+        descriptionRu:
+          "Onomy состоит из четырех столпов, призванных стать самоуправляемой системой денежной стабилизации. Сеть Onomy (ONET): Блокчейн уровня 1 для конкретного приложения, использующий консенсус Tendermint BFT. Обеспечивает двунаправленные мосты с известными блокчейнами в экосистеме Cosmos и за ее пределами, такими как Near, Aurora, Avalanche, Polygon и другими. Onomy Exchange (ONEX): децентрализованная биржа, целью которой является воспроизведение традиционного централизованного обмена для удовлетворения больших объемов мировых запросов в сети, децентрализованно и без хранения. Он сочетает в себе пулы ликвидности AMM с пользовательским интерфейсом книги ордеров, позволяя поставщикам ликвидности получать прибыль, а трейдерам использовать знакомые торговые стратегии книги ордеров, поддерживающие рыночные, лимитные, стоповые и условные ордера. ONEX будет как кроссчейн, так и мультичейн. В сочетании с Onomy Access и мостовой сетью Onomy, ONEX позволяет пользователям беспрепятственно обменивать собственные активы между блокчейнами или торговать активами, присущими одному конкретному блокчейну. Доступ к Onnomy: приложение мобильного кошелька с несколькими цепочками, не связанное с хранением, с помощью которого пользователи могут управлять всеми активами из интегрированных блокчейнов. Это включает в себя стейкинг, управление, передачу активов и даже просмотр ваших коллекций NFT из нескольких блокчейнов — и все это в одном приложении-кошельке. Access упрощает взаимодействие пользователей с несколькими и несколькими цепочками. Подключайтесь к dApps, сканируя QR-код — прошли времена, когда для доступа к Web3 подключались различные расширения браузера. Onnomy Reserve (ORES): управляет чеканкой децентрализованных стейблкоинов под названием Denom, используя NOM в качестве залога. Деномы могут использоваться для торговли иностранной валютой, платежей, денежных переводов, кредитования и расчетов».",
+        blockQuoteEn:
+          "Onomy is bridging Forex and Decentralized Finance through cross-chain liquidity aggregation and digital currencies.",
+        blockQuoteRu:
+          "Onomy объединяет Forex и децентрализованные финансы посредством агрегации ликвидности между цепочками и цифровых валют.",
+        url: "https://onomy.io/",
+        chain: "COSMOS",
+        installationComplexity: "THREE",
+        status: "ACTIVE",
+        rating: "TWO",
+        fieldOfActivity: "Crosschain",
+        valid: true,
+        monthlyPrice: 440,
+        walletAddress: "",
+        imageUrl: "",
+        logo: "",
+        twitter: "https://twitter.com/onomyprotocol",
+        telegram: "https://t.me/onomyprotocol",
+        discord: "https://discord.gg/Wjkbth2q8w",
+        reddit: "",
+        medium: "https://medium.com/onomy-protocol",
+        github: "https://github.com/onomyprotocol",
+      },
+      {
+        id: 6,
+        name: "StarkNet",
+        descriptionEn:
+          "StarkNet is a permissionless ddd decentralized Validity-Rollup (also known as a “ZK-Rollup”). It operates as an L2 network over Ethereum, enabling any dApp to achieve unlimited scale for its computation – without compromising Ethereum’s composability and security, thanks to StarkNet’s reliance on the safest and most scalable cryptographic proof system – STARK. StarkNet Contracts and the StarkNet OS are written in Cairo – supporting the deployment and scaling of any use case, whatever the business logic.",
+        descriptionRu:
+          "StarkNet — это децентрализованный накопительный пакет достоверности DDD без разрешения (также известный как «ZK-накопительный пакет»). Он работает как сеть L2 поверх Ethereum, позволяя любому децентрализованному приложению достигать неограниченного масштаба для своих вычислений — без ущерба для компонуемости и безопасности Ethereum, благодаря тому, что StarkNet полагается на самую безопасную и масштабируемую систему криптографических доказательств — STARK. Контракты StarkNet и ОС StarkNet написаны в Каире, что позволяет развертывать и масштабировать любой вариант использования, независимо от бизнес-логики.",
+        blockQuoteEn:
+          "StarkNet is a permissionless decentralized Validity-Rollup (also known as a “ZK-Rollup”).",
+        blockQuoteRu:
+          "StarkNet — это децентрализованный накопительный пакет достоверности без разрешения (также известный как «ZK-накопительный пакет»).",
+        url: "https://starknet.io/",
+        chain: "L2",
+        installationComplexity: "FIVE",
+        status: "ACTIVE",
+        rating: "FIVE",
+        fieldOfActivity: "ZK",
+        valid: true,
+        monthlyPrice: 310,
+        walletAddress: "",
+        imageUrl: "",
+        logo: "",
+        twitter: "https://twitter.com/Starknet_Intern",
+        telegram: "",
+        discord: "https://discord.gg/KuU6STNAEB",
+        reddit: "",
+        medium: "https://medium.com/starkware/starknet/home",
+        github: "https://github.com/starkware-libs",
+      },
+    ];
+    resolve(shopItems);
+  });
+};
