b7ed14b6c64143308c953865f8a02994152fee4c
Eugen Guriev <gurievcreative@gmail.com>
2023-06-16 17:09:33 +0300

    chore: clean up

--- src/app/components/NewProjects.tsx
+++ src/app/components/NewProjects.tsx
@@ -1,18 +1,3 @@
-import fetchProjects from "../api/fetchProjects";
-import List from "./List";
-
-// const NewProjects = async () => {
-//   const response = await fetchProjects({
-//     limit: "5",
-//     offset: "0",
-//     status: "ACTIVE",
-//   });
-
-//   return <List projects={response.data} />;
-// };
-
-// export default NewProjects;
-
 const NewProjects = () => {
   return <div>NEW PROJECTS</div>;
 };
