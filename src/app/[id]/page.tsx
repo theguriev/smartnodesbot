abc67d73b2cf3352d09aa246f6967df6214ce8cc
Uliana Kolodii <ulianakolodii@gmail.com>
2023-06-23 00:26:43 +0300

    refactor: refactor ItemPage function for improved readability
    - Refactor `ItemPage` function to be an async function
    - Destructure `params` object to obtain `id` directly
    - Remove unused import statement for `FC` from `react`
    - Improve readability by removing unnecessary lines of code
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,14 +1,13 @@
-import { FC } from "react";
 import fetchProject from "../api/fetchProject";
 import ProjectItem from "./components/ProjectItem";
 
-const ItemPage: FC<{ params: { id: number } }> = async ({ params }) => {
-  const { id } = params;
+const ItemPage = async ({ params }: { params: { id: number } }) => {
+  const id = params.id;
   const response = await fetchProject({
     id: id,
   });
 
-  return <ProjectItem project={response} />;
+  return <ProjectItem project={response}/>;
 };
 
 export default ItemPage;
