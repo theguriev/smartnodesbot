37ee115c9c4995094579498029158a2b056a09dd
Eugen Guriev <gurievcreative@gmail.com>
2023-06-22 22:51:13 +0300

    refactor: tabs

--- src/app/components/Tabs.tsx
+++ src/app/components/Tabs.tsx
@@ -3,30 +3,31 @@ import { FC } from "react";
 import t from "@/app/utils/t";
 import Tab from "./Tab";
 
-const tabs = [
-  {
-    href: "/",
-    label: t("Nodes"),
-  },
-  {
-    href: "/testnets",
-    label: t("Testnets"),
-  },
-];
-
-const Tabs: FC<{ path: string }> = ({ path }) => (
-  <div className="flex justify-center mb-4 border-b bg-tg_secondary_bg_color">
-    <ul
-      className="w-[320px] flex flex-wrap -mb-px text-sm font-medium text-center gap-4"
-      id="myTab"
-      data-tabs-toggle="#myTabContent"
-      role="tablist"
-    >
-      {tabs.map(({ href, label }) => (
-        <Tab key={href} href={href} label={label} active={path === href} />
-      ))}
-    </ul>
-  </div>
-);
+const Tabs: FC<{ path: string }> = ({ path }) => {
+  const tabs = [
+    {
+      href: "/",
+      label: t("Nodes"),
+    },
+    {
+      href: "/testnets",
+      label: t("Testnets"),
+    },
+  ];
+  return (
+    <div className="flex justify-center mb-4 border-b bg-tg_secondary_bg_color">
+      <ul
+        className="w-[320px] flex flex-wrap -mb-px text-sm font-medium text-center gap-4"
+        id="myTab"
+        data-tabs-toggle="#myTabContent"
+        role="tablist"
+      >
+        {tabs.map(({ href, label }) => (
+          <Tab key={href} href={href} label={label} active={path === href} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Tabs;
