ad1a0e5a113b9c4a364bedb6f10826461113d133
Uliana Kolodii <ulianakolodii@gmail.com>
2023-06-27 23:21:08 +0300

    refactor: refactor item component for improved image handling
    - Add an image url to the item component
    - Use state to handle image loading errors and show placeholder image
    - Update JSX syntax to use arrow functions
    
    Signed-off-by: Uliana Kolodii <ulianakolodii@gmail.com>

--- src/app/cart/components/Item.tsx
+++ src/app/cart/components/Item.tsx
@@ -1,39 +1,43 @@
-/* eslint-disable @next/next/no-img-element */
-import { FC } from "react";
+"useClient";
+import { FC, useState } from "react";
 import { Project } from "@/app/types";
 
 const Item: FC<
-  Partial<Pick<Project, "name" | "blockQuoteEn" | "monthlyPrice">> & {
+  Partial<
+    Pick<Project, "name" | "blockQuoteEn" | "monthlyPrice" | "imageUrl">
+  > & {
     amount: number;
   }
-> = ({ amount, name, blockQuoteEn, monthlyPrice }) => {
+> = ({ amount, name, blockQuoteEn, monthlyPrice, imageUrl }) => {
   const totalPrice = amount * (monthlyPrice || 0);
+  const [hasError, setHasError] = useState(!imageUrl);
+  const handleImageError = () => {
+    setHasError(true);
+  };
   return (
-    <div
-      className="flex flex-row px-6 py-2 text-sm bg-tg_bg_color"
-    >
-      <div className="flex items-center">
-        <img src="/lava.webp" className="w-10 h-10 mr-3" alt="can't load" />
+    <div className="flex flex-row px-6 py-2 text-sm bg-tg_bg_color">
+      <div>
+        {hasError && (
+          <div className="w-10 h-10 mr-3 rounded-full animate-pulse bg-tg_secondary_bg_color" />
+        )}
+        {!hasError && (
+          <img
+            className="rounded-full cursor-pointer w-10 h-10 mr-3"
+            src={imageUrl}
+            onError={handleImageError}
+            alt="failed"
+          />
+        )}
       </div>
       <div className="w-full flex justify-between">
         <div className="flex flex-col">
           <div className="font-bold flex gap-1">
-            <span
-              className="text-tg_text_color"
-            >
-              {name}
-            </span>
+            <span className="text-tg_text_color">{name}</span>
             <span className="text-smart">{amount}x</span>
           </div>
-          <span className="text-tg_hint_color">
-            {blockQuoteEn}
-          </span>
+          <span className="text-tg_hint_color">{blockQuoteEn}</span>
         </div>
-        <span
-          className="font-semibold text-tg_text_color"
-        >
-          {totalPrice}$
-        </span>
+        <span className="font-semibold text-tg_text_color">{totalPrice}$</span>
       </div>
     </div>
   );
